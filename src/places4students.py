97fe69a84e3e810a25d98aa859dd8841edd3766f
Way05 <wtan235@gmail.com>
2025-11-23 10:59:41 -0500

    adjusted data

--- src/places4students.py
+++ src/places4students.py
@@ -4,22 +4,16 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
-
-import pandas as pd
 import time
 
 url = "https://www.places4students.com/schools/1255/listings/properties?pagination=1"
-
-driver = webdriver.Chrome()
-driver.get(url)
-
 html = None
 data = {
-    "ID": [],
+    # "ID": [],
     "Description": [],
-    "Distance": [],
+    # "Distance": [],
     "Rooms": [],
-    "Washrooms": [],
+    # "Washrooms": [],
     "Price": [],
 }
 
@@ -27,46 +21,46 @@ data = {
 def scrapePage(newSoup):
     listings = newSoup.find_all("div", class_="group")
     listings = filter(None, listings)
-    prices = soup.find_all("div", class_="text-lg font-bold")
+    prices = newSoup.find_all("div", class_="text-lg font-bold")
     count = 0
     for listing in listings:
         desc = listing.find("h3")
         content = listing.find_all("span")
         content = list(filter(lambda c: c.text != "NEW", content))
         if desc and content:
-            data["ID"].append(content[0].text)
+            # data["ID"].append(content[0].text)
             data["Description"].append(desc.text)
-            data["Distance"].append(content[1].text)
+            # data["Distance"].append(content[1].text)
             data["Price"].append(prices[count].text)
             data["Rooms"].append(content[2].text)
-            data["Washrooms"].append(content[3].text)
+            # data["Washrooms"].append(content[3].text)
             count += 1
 
 
-html = driver.page_source
-soup = BeautifulSoup(html, "html.parser")
-pagination = soup.find_all("ul", class_="flex flex-row items-center gap-1")
-pagination = pagination[0].find_all("li")[1:-1]
-for i in range(1, len(pagination) + 1):
-    try:
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.LINK_TEXT, str(i)))
-        )
-        driver.find_element(By.LINK_TEXT, str(i)).click()
+def getData():
+    driver = webdriver.Chrome()
+    driver.get(url)
+    html = driver.page_source
+    soup = BeautifulSoup(html, "html.parser")
+    pagination = soup.find_all("ul", class_="flex flex-row items-center gap-1")
+    pagination = pagination[0].find_all("li")[1:-1]
+    for i in range(1, len(pagination) + 1):
+        try:
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.LINK_TEXT, str(i)))
+            )
+            driver.find_element(By.LINK_TEXT, str(i)).click()
 
-        time.sleep(2)
+            time.sleep(3)
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, "html.parser")
-        scrapePage(soup)
-    except NoSuchElementException:
-        print("End of pages.")
-        break
+            html = driver.page_source
+            soup = BeautifulSoup(html, "html.parser")
+            scrapePage(soup)
+        except NoSuchElementException:
+            print("End of pages.")
+            break
 
-driver.switch_to.default_content()
-driver.quit()
+    driver.switch_to.default_content()
+    driver.quit()
 
-df = pd.DataFrame(
-    data,
-)
-print(df)
+    return data
--- src/udstudentrentals.py
+++ src/udstudentrentals.py
@@ -4,61 +4,57 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
-import pandas as pd
 import time
 
 url = "https://www.udstudentrentals.com/availableproperties"
-
-driver = webdriver.Chrome()
-driver.get(url)
-
 html = None
 data = {"Location": [], "Price": [], "Tenants": [], "Status": []}
 
-try:
-    time.sleep(1)
-    scroll_amount = 1000  # Adjust this value based on your page's layout
-    driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
-    print(f"Scrolled iframe document down by {scroll_amount} pixels.")
 
-    time.sleep(1)
+def getData():
+    driver = webdriver.Chrome()
+    driver.get(url)
+    try:
+        time.sleep(1)
+        scroll_amount = 1000  # Adjust this value based on your page's layout
+        driver.execute_script(f"window.scrollBy(0, {scroll_amount});")
+        # print(f"Scrolled iframe document down by {scroll_amount} pixels.")
 
-    iframe = WebDriverWait(driver, 10).until(
-        EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "nKphmK"))
-    )
-    print("iframe found")
-    # driver.switch_to.frame(iframe)
-    print("switched to iframe")
+        time.sleep(1)
 
-    time.sleep(1)
+        iframe = WebDriverWait(driver, 10).until(
+            EC.frame_to_be_available_and_switch_to_it((By.CLASS_NAME, "nKphmK"))
+        )
+        # print("iframe found")
+        # driver.switch_to.frame(iframe)
+        # print("switched to iframe")
 
-    dataTable = WebDriverWait(driver, 10).until(
-        # EC.visibility_of_element_located((By.ID, "theTable"))
-        # EC.visibility_of_element_located((By.XPATH, "//*[@id='theTable']"))
-        EC.presence_of_element_located((By.XPATH, "//*[@id='theTable']//tbody"))
-    )
-    print("table found")
+        time.sleep(1)
 
-    html = driver.page_source
+        dataTable = WebDriverWait(driver, 10).until(
+            # EC.visibility_of_element_located((By.ID, "theTable"))
+            # EC.visibility_of_element_located((By.XPATH, "//*[@id='theTable']"))
+            EC.presence_of_element_located((By.XPATH, "//*[@id='theTable']//tbody"))
+        )
+        # print("table found")
 
-except TimeoutException:
-    print("Failed to load in time.")
+        html = driver.page_source
 
-driver.switch_to.default_content()
-driver.quit()
+    except TimeoutException:
+        print("Failed to load in time.")
 
-if html:
-    soup = BeautifulSoup(html, "html.parser")
-    listings = soup.find_all("tr")
-    for i in range(len(listings)):
-        details = listings[i].find_all("td")
-        for i in range(0, len(details) - 3, 4):
-            data["Location"].append(details[i].text)
-            data["Price"].append(details[i + 1].text)
-            data["Tenants"].append(details[i + 2].text)
-            data["Status"].append(details[i + 3].text)
+    driver.switch_to.default_content()
+    driver.quit()
 
-    df = pd.DataFrame(
-        data,
-    )
-    print(df)
+    if html:
+        soup = BeautifulSoup(html, "html.parser")
+        listings = soup.find_all("tr")
+        for i in range(len(listings)):
+            details = listings[i].find_all("td")
+            for i in range(0, len(details) - 3, 4):
+                data["Location"].append(details[i].text)
+                data["Price"].append(details[i + 1].text)
+                data["Tenants"].append(details[i + 2].text)
+                data["Status"].append(details[i + 3].text)
+
+    return data
