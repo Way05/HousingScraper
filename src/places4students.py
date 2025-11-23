ef067e96cfd0b8d2050e54cccdb17c1db979e969
Way05 <wtan235@gmail.com>
2025-11-23 00:27:31 -0500

    working pagination

--- src/places4students.py
+++ src/places4students.py
@@ -1,7 +1,12 @@
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
+
 import pandas as pd
+import time
 
 url = "https://www.places4students.com/schools/1255/listings/properties?pagination=1"
 
@@ -18,18 +23,8 @@ data = {
     "Price": [],
 }
 
-try:
-    html = driver.page_source
-
-except TimeoutException:
-    print("Failed to load in time.")
-
-driver.switch_to.default_content()
-driver.quit()
-
-if html:
-    soup = BeautifulSoup(html, "html.parser")
-    listings = soup.find_all("div", class_="group")
+def scrapePage(newSoup):
+    listings = newSoup.find_all("div", class_="group")
     listings = filter(None, listings)
     for listing in listings:
         desc = listing.find("h3")
@@ -43,7 +38,30 @@ if html:
             data["Rooms"].append(content[2].text)
             data["Washrooms"].append(content[3].text)
 
-    df = pd.DataFrame(
-        data,
-    )
-    print(df)
+html = driver.page_source
+soup = BeautifulSoup(html, "html.parser")
+pagination = soup.find_all("ul", class_="flex flex-row items-center gap-1")
+pagination = pagination[0].find_all("li")[1:-1]
+for i in range(1, len(pagination) + 1):
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.LINK_TEXT, str(i)))
+        )
+        driver.find_element(By.LINK_TEXT, str(i)).click()
+
+        time.sleep(2)
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        scrapePage(soup)
+    except NoSuchElementException:
+        print("End of pages.")
+        break
+
+driver.switch_to.default_content()
+driver.quit()
+
+df = pd.DataFrame(
+    data,
+)
+print(df)
