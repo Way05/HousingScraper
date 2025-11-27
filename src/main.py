29a45e3d6ad75c06a82f7ecce439ae216e95aedc
Wei-en Tan <wtan235@gmail.com>
2025-11-26 20:22:14 -0500

    some command setup

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ print("1. UD Student Rentals")
 print("2. Places4Students")
 print("3. All of the above")
 print("4. Exit")
-input = input("Enter a number: ")
+choice = input("Enter a number: ")
 
 data = None
 dfs = []
@@ -23,34 +23,49 @@ def formatPrice(df):
     df.sort_values(by=["Price ($)"], inplace=True)
 
 
-match input:
-    case "1":
-        data = udr()
-        df = pd.DataFrame(data)
-        formatPrice(df)
-        dfs.append(df)
-        pass
-    case "2":
-        data = p4s()
-        df = pd.DataFrame(data)
-        formatPrice(df)
-        dfs.append(df)
-        pass
-    case "3":
-        data1 = udr()
-        df1 = pd.DataFrame(data1)
-        data2 = p4s()
-        df2 = pd.DataFrame(data2)
-        formatPrice(df1)
-        formatPrice(df2)
-        dfs.append(df1)
-        dfs.append(df2)
-        pass
-    case "4":
-        print("Exiting...")
+def fetchData():
+    match choice:
+        case "1":
+            data = udr()
+            df = pd.DataFrame(data)
+            formatPrice(df)
+            dfs.append(df)
+        case "2":
+            data = p4s()
+            df = pd.DataFrame(data)
+            formatPrice(df)
+            dfs.append(df)
+        case "3":
+            data1 = udr()
+            df1 = pd.DataFrame(data1)
+            data2 = p4s()
+            df2 = pd.DataFrame(data2)
+            formatPrice(df1)
+            formatPrice(df2)
+            dfs.append(df1)
+            dfs.append(df2)
+        case "4":
+            print("Exiting...")
 
-if dfs is None:
-    print("No results found.")
-else:
-    for df in dfs:
-        print(df)
+
+while True:
+    cmd = input(
+        "Enter CMD: (D) Display, (E) Export, (F) Filter, (S) Sort, (Q) Quit"
+    ).upper()
+    match cmd:
+        case "D":
+            if dfs is None:
+                print("No results found.")
+            else:
+                for df in dfs:
+                    print(df)
+        case "E":
+            pass
+        case "F":
+            pass
+        case "S":
+            pass
+        case "Q":
+            break
+        case _:
+            print("Unrecognized Command, try again.")
