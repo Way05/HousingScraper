243ca2feaa88ef0e4c0f0776ae96a03d1c8e27a6
Wei-en Tan <wtan235@gmail.com>
2025-11-30 00:04:29 -0500

    dfs merged and can sort by price

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ import os
 run = True
 dfs = []
 
+
 os.system("cls")
 
 
@@ -20,6 +21,9 @@ def formatData(*raw_data):
             )
             df.sort_values(by=["Price ($)"], inplace=True, ignore_index=True)
             dfs.append(df)
+    merged_df = pd.concat([*dfs], ignore_index=True)
+    merged_df.dropna()
+    return merged_df
 
 
 def fetchData():
@@ -36,36 +40,37 @@ def fetchData():
         case "1":
             print("Fetching data...")
             data = udr()
-            formatData(data)
+            merged_df = formatData(data)
         case "2":
             print("Fetching data...")
             data = p4s(name)
-            formatData(data)
+            merged_df = formatData(data)
         case "3":
             print("Fetching data...")
             data1 = udr()
             data2 = p4s(name)
-            formatData(data1, data2)
+            merged_df = formatData(data1, data2)
         case _:
             print("Exiting...")
-            return False
+            return None
     print("Data fetched.")
-    return True
+    return merged_df
 
 
-run = fetchData()
+merged_df = fetchData()
 
-while True and run:
+while True and merged_df is not None:
     cmd = input(
         "Enter CMD: (D) Display, (E) Export, (F) Filter, (S) Sort, (Q) Quit\n"
     ).upper()
     match cmd:
         case "D":
-            if dfs is None:
-                print("No results found.")
-            else:
-                for df in dfs:
-                    print(df)
+            # if dfs is None:
+            #     print("No results found.")
+            # else:
+            #     for df in dfs:
+            #         print(df)
+            print(merged_df)
         case "E":
             if len(dfs) > 1:
                 ask = input("(S) Separate or (T) together?: ").upper()
@@ -76,30 +81,51 @@ while True and run:
                         df.to_csv(f"out_0{count}.csv", index=False)
                         count += 1
                 elif ask == "T":
-                    # merged_df = pd.merge(
-                    #     dfs[0],
-                    #     dfs[1],
-                    #     on=[
-                    #         "Description",
-                    #         "Price ($)",
-                    #         "Bedrooms",
-                    #         "Bathrooms",
-                    #         "Link",
-                    #     ],
-                    # )
-                    # merged_df.to_csv("merged_out.csv", index=False)
-                    merged_df = pd.concat([dfs[0], dfs[1]], ignore_index=True)
-                    merged_df = merged_df.dropna()
                     merged_df.to_csv("merged_out.csv", index=False)
                 else:
                     print("Invalid.")
             else:
-                df.to_csv("out_01.csv", index=False)
+                merged_df.to_csv("out_01.csv", index=False)
             print("Export complete.")
         case "F":
-            pass
+            filter_by = input(
+                "What do you want to filter? (P) Price, (R) Rooms, (B) Bathrooms: "
+            ).upper()
+            match filter_by:
+                case "P":
+                    pass
+                case "R":
+                    pass
+                case "B":
+                    pass
+                case _:
+                    print("Invalid.")
         case "S":
-            pass
+            sort_by = input(
+                "What do you want to sort? (P) Price, (R) Rooms, (B) Bathrooms: "
+            )
+            match sort_by:
+                case "P":
+                    order = input("(A) Ascending // (D) Descending: ").upper()
+                    if order == "A":
+                        merged_df.sort_values(
+                            by=["Price ($)"], inplace=True, ignore_index=True
+                        )
+                    elif order == "D":
+                        merged_df.sort_values(
+                            by=["Price ($)"],
+                            ascending=False,
+                            inplace=True,
+                            ignore_index=True,
+                        )
+                    else:
+                        print("Invalid.")
+                case "R":
+                    pass
+                case "B":
+                    pass
+                case _:
+                    print("Invalid.")
         case "Q":
             print("Exiting...")
             break
